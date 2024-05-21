f28324614b56b067e7c8d9b4656037e22040e5fa
unknown <pschmidt@pasit.nl>
2024-05-21 13:08:38 +0200

    first

---
+++ TafelOef2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace TafelOef2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int tafel;
+        do
+        {
+            Console.WriteLine("Welke tafel? (getal tussen 1 en 100)");
+            string stafel = Console.ReadLine();
+
+            if (!int.TryParse(stafel, out tafel))
+            {
+                Console.WriteLine("Ongeldige invoer");
+            }
+            if (tafel <= 0 || tafel > 100)
+            {
+                Console.WriteLine("Geen geldige tafel");
+            }
+            else
+            {
+                break;
+            }
+        }
+        while (true);
+
+
+        Console.WriteLine($"De tafel van {tafel}");
+        for (int teller = 1; teller <= 10; teller++)
+        {
+            Console.WriteLine($"{teller} x {tafel} = {teller * tafel}");
+        }
+    }
+}
+
